cacf0309b42b2f3849fdf2fee7323ad4aa807d0f
Black-Panther30121995 <shivanikumari20050320@gmail.com>
2025-04-30 12:04:42 +0530

    refining file

--- Book App/frontend/src/App.jsx
+++ Book App/frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Routes,Route} from 'react-router-dom';
+import { Link, Routes, Route } from 'react-router-dom';
 import AddBook from './components/AddBook';
 import SearchBook from './components/SearchBook';
 import UpdateBook from './components/UpdateBook';
@@ -20,6 +20,12 @@ const App = () => {
           <Link to="/delete" className="navbar-link">Delete Book</Link>
         </div>
       </nav>
+      <img
+        style={{ display: "block", margin: "20px auto", maxWidth: "100%", height: "auto" }}
+        src="https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg?semt=ais_hybrid&w=740"
+        alt="Books"
+      />
+
       <Routes>
         <Route path="/search" element={<SearchBook />} />
         <Route path="/view" element={<ViewBook />} />
